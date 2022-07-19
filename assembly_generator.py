b6fb2edd0fc7eba459c52a52aef6edf71d8194e9
Nikita Osovskiy <nikitaosovskiy2005ok181@gmail.com>
2022-07-19 12:41:56 +0800

    Complete Unary Operators

--- assembly_generator.py
+++ assembly_generator.py
@@ -21,9 +21,20 @@ class AssemblyGenerator:
 
     def _generate_statement(self) -> str:
         if type(self._tree).__name__ == 'Return':
+            return f'{self._generate_return()} ret \n'
+
+    def _generate_return(self) -> str:
             self._tree = self._tree.child
             if type(self._tree).__name__ == 'Constant':
-                return f' movl\t${self._tree.value}, %eax\n ret\n'
+                return f' movl\t${self._tree.value}, %eax\n'
+            if type(self._tree).__name__ == 'UnaryOperator':
+                if self._tree.operator == '-':
+                    return f'{self._generate_return()} neg %eax\n'
+                elif self._tree.operator == '~':
+                    return f'{self._generate_return()} not %eax\n'
+                elif self._tree.operator == '!':
+                    return f'{self._generate_return()} cmpl $0, %eax\n movl $0, %eax\n sete %al\n'
+
 
 def check_generator(input_file):
     s = open(input_file).read()
@@ -33,4 +44,4 @@ def check_generator(input_file):
     print(gen._generate_code())
 
 if __name__ == '__main__':
-    check_generator('return_2.c')
+    check_generator('nested_ops.c')
